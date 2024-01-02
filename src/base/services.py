89df0c85d182f55568072c937204a965d4e053d7
marik <letschuka@gmail.com>
2024-01-02 21:20:11 +0200

    add models to oath and add ckeck avatar functions

---
+++ src/base/services.py
@@ -0,0 +1,15 @@
+from rest_framework.exceptions import ValidationError
+
+
+def get_path_upload_avatar(instance, file):
+    """ Path creation, format: (media)/avatar/user_id/photo.jpg
+    """
+
+    return f'avatar/{instance.id}/{file}'
+
+def validate_size_image(file_obj):
+    """ Check size of file
+    """
+    megabyte_limit = 2
+    if file_obj.size > megabyte_limit * 1024 * 1024:
+        raise ValidationError(f"Max size of file {megabyte_limit}MB")
--- src/oauth/models.py
+++ src/oauth/models.py
@@ -1,3 +1,49 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
-# Create your models here.
+from src.base.services import get_path_upload_avatar, validate_size_image
+
+
+class AuthUser(models.Model):
+    """ User model on my platform
+    """
+    email = models.EmailField(max_length=150, unique=True)
+    join_date = models.DateField(auto_now_add=True)
+    country = models.CharField(max_length=30, blank=True, null=True)
+    city = models.CharField(max_length=30, blank=True, null=True)
+    bio = models.TextField(max_length=1500, blank=True, null=True)
+    display_name = models.CharField(max_length=30, blank=True, null=True)
+    avatar = models.ImageField(
+        upload_to=get_path_upload_avatar,
+        blank=True,
+        null=True,
+        validators=[FileExtensionValidator(allowed_extensions=['jpg']), validate_size_image]
+    )
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def is_authenticated(self):
+        return True
+
+
+class Follower(models.Model):
+    """ Model of subscribers
+    """
+    user = models.ForeignKey(AuthUser, on_delete=models.CASCADE, related_name='owner')
+    subscriber = models.ForeignKey(AuthUser, on_delete=models.CASCADE, related_name='subscribers')
+
+    def __str__(self):
+        return f'{self.subscriber} subscribe on {self.user}'
+
+
+class SocialLink(models.Model):
+    """ Model links on social network of user
+    """
+    user = models.ForeignKey(AuthUser, on_delete=models.CASCADE, related_name='social_links')
+    link = models.URLField(max_length=100)
+
+    def __str__(self):
+        return f'self.user'
+
