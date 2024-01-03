65a3d088230856ee0af988c070440131cf04f6a2
marik <letschuka@gmail.com>
2024-01-03 13:34:39 +0200

    adjust admin panel and update requirements.txt

--- SoundApplication/settings.py
+++ SoundApplication/settings.py
@@ -62,8 +62,12 @@ WSGI_APPLICATION = 'SoundApplication.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('SQL_ENGINE'),
+        'NAME': os.getenv('SQL_NAME'),
+        'USER': os.getenv('SQL_USER'),
+        'PASSWORD': os.getenv('SQL_PASSWORD'),
+        'HOST': os.getenv('SQL_HOST'),
+        'PORT': os.getenv('SQL_PORT'),
     }
 }
 
