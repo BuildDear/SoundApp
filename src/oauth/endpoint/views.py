e2558713044f391b7c0fabc100f4b19b6fe87c41
marik <letschuka@gmail.com>
2024-01-05 23:31:13 +0200

    add UserView and override some fields and methods

--- src/oauth/endpoint/views.py
+++ src/oauth/endpoint/views.py
@@ -1,7 +1,17 @@
-from rest_framework import viewsets, parsers
+from rest_framework import viewsets, parsers, permissions
+
+from src.oauth import serializer
+
 
 class UserView(viewsets.ModelViewSet):
     """ View and edit user data
     """
     parser_classes = (parsers.MultiPartParser,)
-    serializer_class =
+    serializer_class = serializer.UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return self.request.user
+
+    def get_object(self):
+        return self.get_queryset()
