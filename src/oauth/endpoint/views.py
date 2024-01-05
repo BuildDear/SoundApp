297297b19fd8ed802b143fccfce9eb4e7037f8c7
marik <letschuka@gmail.com>
2024-01-05 23:22:13 +0200

    add user serializer

--- src/oauth/endpoint/views.py
+++ src/oauth/endpoint/views.py
@@ -1,2 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, parsers
 
+class UserView(viewsets.ModelViewSet):
+    """ View and edit user data
+    """
+    parser_classes = (parsers.MultiPartParser,)
+    serializer_class =
--- src/oauth/serializer.py
+++ src/oauth/serializer.py
@@ -1,5 +1,13 @@
 from rest_framework import serializers
 
+from . import models
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.AuthUser
+        fields = ('avatar', 'country', 'city', 'bio', 'display_name')
+
 
 class GoogleAuth(serializers.Serializer):
     email = serializers.EmailField()
