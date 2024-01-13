4c642c932915b5dffb7edcd38b6e1078804a6933
marik <letschuka@gmail.com>
2024-01-13 21:05:48 +0200

    update functions returns and add album serializer

--- src/audio_lib/serializer.py
+++ src/audio_lib/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from . import models
+from ..base.services import delete_old_file
 
 
 class BaseSerializer(serializers.ModelSerializer):
@@ -16,3 +17,15 @@ class LicenseSerializer(BaseSerializer):
     class Meta:
         model = models.License
         fields = ('id', 'text')
+
+
+class AlbumSerializer(BaseSerializer):
+    class Meta:
+        model = models.Album
+        fields = ('id', 'name', 'description', 'cover', 'private')
+
+        def update(self, instance, validated_data):
+            delete_old_file(instance.cover.path)
+            return super().update(instance, validated_data)
+
+
--- src/base/services.py
+++ src/base/services.py
@@ -1,28 +1,30 @@
+import os
+
 from rest_framework.exceptions import ValidationError
 
 
 def get_path_upload_avatar(instance, file):
     """ Path creation, format: (media)/avatar/user_id/photo.jpg
     """
-    return f'avatar/user_{instance.id}/{file}'
+    return f'avatar/user_{instance.user.id}/{file}'
 
 
 def get_path_upload_cover_album(instance, file):
     """ Path creation, format: (media)/album/user_id/photo.jpg
     """
-    return f'album/user_{instance.id}/{file}'
+    return f'album/user_{instance.user.id}/{file}'
 
 
 def get_path_upload_track(instance, file):
     """ Path creation, format: (media)/track/user_id/photo.jpg
     """
-    return f'track/user_{instance.id}/{file}'
+    return f'track/user_{instance.user.id}/{file}'
 
 
 def get_path_upload_cover_playlist(instance, file):
     """ Path creation, format: (media)/playlist/user_id/photo.jpg
     """
-    return f'playlist/user_{instance.id}/{file}'
+    return f'playlist/user_{instance.user.id}/{file}'
 
 
 def validate_size_image(file_obj):
@@ -31,3 +33,10 @@ def validate_size_image(file_obj):
     megabyte_limit = 2
     if file_obj.size > megabyte_limit * 1024 * 1024:
         raise ValidationError(f"Max size of file {megabyte_limit}MB")
+
+
+def delete_old_file(path_file):
+    """ Delete old file
+    """
+    if os.path.exists(path_file):
+        os.remove(path_file)
